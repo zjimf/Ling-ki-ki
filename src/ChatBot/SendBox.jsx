1afe3b92ca69c43cfe0eafa4c41e989cfeba3388
zjimf <jmjmjim1101@gmail.com>
2024-05-19 10:19:44 +0800

    Adding Loading

--- src/ChatBot/SendBox.jsx
+++ src/ChatBot/SendBox.jsx
@@ -1,11 +1,58 @@
 import React, { useState } from "react";
-import { Box } from "@mui/material";
+import { Box, CircularProgress } from "@mui/material";
 import TextField from "@mui/material/TextField";
 import IconButton from "@mui/material/IconButton";
 import SendIcon from "@mui/icons-material/Send";
 
 const SendBox = ({ chatMessages, setChatMessages }) => {
   const [msg, setMsg] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  const sendMessage = () => {
+    const newMessage = {
+      position: "right",
+      type: "text",
+      title: "You",
+      text: msg,
+    };
+
+    if (msg.trim()) {
+      setChatMessages([...chatMessages, newMessage]);
+      setMsg("");
+    }
+
+    setIsLoading(true);
+    fetch("http://localhost:8000/chat", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ message: msg }),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        const botMessageText = data.reply.split("\n").map((item, key) => (
+          <React.Fragment key={key}>
+            {item}
+            <br />
+          </React.Fragment>
+        ));
+
+        const botMessage = {
+          position: "left",
+          type: "text",
+          title: "智能小管家",
+          text: botMessageText,
+        };
+        setChatMessages((prevMessages) => [...prevMessages, botMessage]);
+      })
+      .catch((error) => {
+        console.error("Error:", error);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+  };
 
   return (
     <Box
@@ -23,53 +70,13 @@ const SendBox = ({ chatMessages, setChatMessages }) => {
         sx={{ width: "80vw" }}
       />
       <IconButton
-        onClick={(e) => {
-          const newMessage = {
-            position: "right",
-            type: "text",
-            title: "You",
-            text: msg,
-          };
-
-          if (msg.trim()) {
-            setChatMessages([...chatMessages, newMessage]);
-            setMsg("");
-          }
-
-          fetch("http://localhost:8000/chat", {
-            method: "POST",
-            headers: {
-              "Content-Type": "application/json",
-            },
-            body: JSON.stringify({ message: msg }),
-          })
-            .then((response) => response.json())
-            .then((data) => {
-              const botMessageText = data.reply.split("\n").map((item, key) => (
-                <React.Fragment key={key}>
-                  {item}
-                  <br />
-                </React.Fragment>
-              ));
-
-              const botMessage = {
-                position: "left",
-                type: "text",
-                title: "智能小管家",
-                text: botMessageText,
-              };
-              setChatMessages((prevMessages) => [...prevMessages, botMessage]);
-            })
-            .catch((error) => {
-              console.error("Error:", error);
-            });
-        }}
+        onClick={sendMessage}
         sx={{ marginLeft: "10px" }}
         color="primary"
         aria-label="send message"
         size="large"
       >
-        <SendIcon />
+        {isLoading ? <CircularProgress size={24} /> : <SendIcon />}
       </IconButton>
     </Box>
   );
